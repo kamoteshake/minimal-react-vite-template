901c2f511abecf807bae1ff483e8c6e0b7586a11
Kamoteshake <kamoteshake@gmail.com>
2025-09-26 01:32:47 -0500

    setup mui and react router

---
+++ src/Home.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+
+import reactLogo from 'assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const Home: React.FC = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Box
+      width="100%"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Box display="flex">
+        <Box mx={4}>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} alt="Vite logo" style={{ width: 150 }} />
+          </a>
+        </Box>
+        <Box mx={4}>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} alt="React logo" style={{ width: 150 }} />
+          </a>
+        </Box>
+      </Box>
+      <Typography variant="h1">Vite + React</Typography>
+      <Box my={1}>
+        <Button variant="contained" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+      </Box>
+      <Box my={1}>
+        <Typography variant="body1">
+          Edit <code>src/Home.tsx</code> and save to test HMR
+        </Typography>
+      </Box>
+      <Box my={1}>
+        <Typography variant="caption">Click on the Vite and React logos to learn more</Typography>
+      </Box>
+    </Box>
+  )
+}
+
+export default Home
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+import theme from './theme.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </StrictMode>
 )
